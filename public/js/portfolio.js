06f814a65e2f45cc3e5a8ea3064d0ebfac3a7061
Brenden Miller <bmiller@Hes.Tree>
2019-04-24 16:08:44 -0700

    Portfolio Rework

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,4 +1,37 @@
 $(".navTab").click(function(){
   $(".tab").removeClass("active");
+  $(".navTab").removeClass("active");
   $('#' + $(this).attr("dest")).toggleClass("active");
+  $('#' + $(this).attr("dest")+ 'Nav').toggleClass("active");
+  window.scrollTo(0,0);
 });
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" activeSlide", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " activeSlide";
+}
